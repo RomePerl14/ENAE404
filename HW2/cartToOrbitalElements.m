b38e4d75dcb86d5158d42d072609db55c6416e09
Romeo-Perl-14 <romeoperlstein14@gmail.com>
2024-02-28 23:24:32 -0500

    balling

--- HW2/cartToOrbitalElements.m
+++ HW2/cartToOrbitalElements.m
@@ -1,7 +1,6 @@
-
+% Convert Cartesian state to PQW state values
 function [i_param_, omega_param_, w_param_, true_anom_, e_x_, e_y_, e_z_, a_, spef_energy_] = cartToOrbitalElements(position_vector_, velocity_vector_, mew_)
     format long
-    fprintf("Expecting 6 states, xyz and vz,vy,vz as inputs... better see it!")
     if(isempty(position_vector_))
         fprintf("How tf you do that? You need to input a vector of length 3")
     elif(length(position_vector_) == 1)
@@ -32,7 +31,7 @@ function [i_param_, omega_param_, w_param_, true_anom_, e_x_, e_y_, e_z_, a_, sp
     n_hat = n/norm_n; % unit vector of the node vector
     nx = n(1); % n vector x
     ny = n(2); % n vector y
-    nx = (n3); % n vector z
+    nx = n(3); % n vector z
     
     % Get the eccentricity
     e = (cross(velocity_vector_,h)/mew_) - (position_vector_/norm(position_vector_)); % eccentricity vector
@@ -64,54 +63,49 @@ function [i_param_, omega_param_, w_param_, true_anom_, e_x_, e_y_, e_z_, a_, sp
     % Is this necessary? not really, but does it help? no. But it makes it
     % faster right? no, it probably makes it slower in all honesty. But it
     % will at least be correct, right? yes! (probably not actually)
-    if(norm_n == 0)
-        if (norm_e == 0) % check if orbit is circular, equitorial
-            i_param_ = 0;
-            w_param_= 0;
-            omega_param_ = 0;
-            true_anom_ = acos(rx/norm_r); % get the true anomaly
-            if(ry < 0) % check condition
-                true_anom_ = 2*pi - true_anom_; % rewrite the true anomaly to account for shift
-            end
-        elseif(norm_e > 0 && norm_e < 1) % check if orbit is elliptical, equitorial
-            i_param_ = 0;
-            w_param_ = acos(e_x_/norm_e);
-            if(e_y_ < 0) % check condition
-                w_param_ = 2*pi-w_param_; % rewrite the w param to account for shift
-            end
-            omega_param_ = 0;
-            true_anom_ = acos((dot(e, position_vector_))/(norm_e*norm_r));
-            if(dot(position_vector_, velocity_vector_) < 0)
-                true_anom_ = 2*pi-true_anom_; % rewrite the true anomaly to account for shift
-            end
+    if(norm_n == 0 && norm_e == 0) % check if orbit is circular, equitorial
+        i_param_ = 0;
+        w_param_= 0;
+        omega_param_ = 0;
+        true_anom_ = acos(rx/norm_r); % get the true anomaly
+        if(ry < 0) % check condition
+            true_anom_ = (2*pi)-true_anom_; % rewrite the true anomaly to account for shift
         end
-    elseif(norm_n > 0)
-        if(norm_e == 0) % check if orbit is circular inclined
-            i_param_ = acos(hz/norm_h);
-            w_param_ = 0;
-            omega_param_ = acos(nx/norm_n);
-            if(ny < 0) % check condition
-                omega_param_ = 2*pi - omega_param_; % rewrite the omega param to account for shift
-            end
-            true_anom_ = acos((dot(n, position_vector_))/(norm_n*norm_pos));
-            if(rz < 0) % check condition
-                true_anom_ = 2*pi-true_anom_; % rewrite the true anomaly to account for shift
-            end
-        elseif(norm_e > 0) % check if orbit is elliptical inclined
-            i_param_ = acos(hz/norm_h);
-            w_param_ = acos((dot(n,e))/(norm_n*norm_e));
-            if(e_z_ < 0)
-                w_param_ = 2*pi-w_param_; % rewrite w param to account for shift
-            end
-            omega_param_ = acos(nx/norm_n);
-            if(ny < 0)
-                omega_param_ = 2*pi-omega_param_; % rewrite omega param to account for shift
-            end
-            true_anom_ = acos((dot(e,r))/(norm_e*norm_pos));
-            if(dot(position_vector_,velocity_vector_) < 0)
-                true_anom_ = 2*pi-true_anom_; % rewrite true anomaly to account for shift
-            end
+    elseif(norm_n == 0 && (norm_e < 1 && norm_e > 0)) % check if orbit is elliptical, equitorial
+        i_param_ = 0;
+        w_param_ = acos(e_x_/norm_e);
+        if(e_y_ < 0) % check condition
+            w_param_ = (2*pi)-w_param_; % rewrite the w param to account for shift
         end
-
+        omega_param_ = 0;
+        true_anom_ = acos((dot(e, position_vector_))/(norm_e*norm_r));
+        if(dot(position_vector_, velocity_vector_) < 0)
+            true_anom_ = (2*pi)-true_anom_; % rewrite the true anomaly to account for shift
+        end
+    elseif(norm_n > 0 && norm_e == 0) % check if orbit is circular inclined
+        i_param_ = acos(hz/norm_h);
+        w_param_ = 0;
+        omega_param_ = acos(nx/norm_n);
+        if(ny < 0) % check condition
+            omega_param_ = (2*pi)-omega_param_; % rewrite the omega param to account for shift
+        end
+        true_anom_ = acos((dot(n, position_vector_))/(norm_n*norm_pos));
+        if(rz < 0) % check condition
+            true_anom_ = (2*pi)-true_anom_; % rewrite the true anomaly to account for shift
+        end
+    elseif(norm_n > 0 && norm_e > 0) % check if orbit is elliptical inclined
+        i_param_ = acos(hz/norm_h);
+        w_param_ = acos((dot(n,e))/(norm_n*norm_e));
+        if(e_z_ < 0)
+            w_param_ = (2*pi)-w_param_; % rewrite w param to account for shift
+        end
+        omega_param_ = acos(nx/norm_n);
+        if(ny < 0)
+            omega_param_ = (2*pi)-omega_param_; % rewrite omega param to account for shift
+        end
+        true_anom_ = acos((dot(e,position_vector_))/(norm_e*norm_pos));
+        if(dot(position_vector_,velocity_vector_) < 0)
+            true_anom_ = (2*pi)-true_anom_; % rewrite true anomaly to account for shift
+        end
     end
 end
---
+++ HW2/orbitalElementsToCart.m
@@ -0,0 +1,11 @@
+% Convert PQW states to Cartesian states
+function [position_vec_, velocity_vec_, spef_enegry_] = orbitalElementsToCart(a_, e_, i_param_, w_param_, omega_param_, true_anom_, mew_)
+    % find r_pqw_vec  
+
+
+
+
+
+
+
+end
