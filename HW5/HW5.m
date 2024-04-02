73e587113194fe797adb6c4fff10e77a52d5319b
Romeo-Perl-14 <romeoperlstein14@gmail.com>
2024-04-02 00:05:27 -0400

    its jover

---
+++ HW5/HW5.m
@@ -0,0 +1,16 @@
+%-------- HW 5 MATLAB code --------%
+% Romeo Perlstein, section 0101 %
+
+% IT'S A NEW WEEK - HOPEFULLY I CAN KEEP UP THE WORK AND NOT FALL BEHIND!
+
+%% Q1
+% Find deltaV for Mercury to Jupiter transfer using conic sections
+
+% Givens:
+mew_mercury = 22031.868551; % km^3/s^2 - FROM JPL
+mew_saturn = 126712764.1; % km^3/s^2 - FROM JPL
+r_craft_mercury = 400; % km
+r_mercury
+
+r_craft_saturn = 10000; % km
+r_saturn
