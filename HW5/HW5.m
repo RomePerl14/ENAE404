b2781fd4cf5b5c95b74045fb4f50995981e179af
romeo <romeoperlstein14@gmail.com>
2024-04-13 15:51:32 -0400

    balling at a good cost finally

--- HW5/HW5.m
+++ HW5/HW5.m
@@ -130,5 +130,115 @@ v_finalC_mag = norm(v_finalC);
 
 
 %% Q3
+% solve keplers problamo
+%%% spacecraft 1
+mew_earth = 0.39860*10^6;
+a1 = 15000;
+e1 = 0.4;
+i1 = 60;
+Omega1 = 45;
+w1 = 0;
+true_anom1 = 145;
+deltaT = 3600; % seconds in an hour
+ 
+% first, find E1_0 and mean motion
+E1_0 = acos((e1+cosd(true_anom1))/(1+e1*cosd(true_anom1))*pi/180);
+n = sqrt((a1^3)/mew_earth);
+
+% now, solve the equation for E and get a constant
+% Assuming k = 0 for simplicity? not sure how to solve for two unknowns
+k = 0;
+C1 = deltaT/n - 2*pi*k + (E1_0 - e1*sin(E1_0));
+E1_guess = pi; % initial guess
+fx1 = C1 - E1_guess + e1*sin(E1_guess); % begin iteration to solve for E
+fx1_prime = -1 + e1*E1_guess*cos(E1_guess);
+E1_prev = E1_guess - fx1/fx1_prime;
+for ii=1:1:100 % 100 iterations should do the trick
+    fx1 = C1 - E1_prev + e1*sin(E1_prev); % begin iteration to solve for E
+    fx1_prime = -1 + e1*E1_prev*cos(E1_prev);
+    E1_new = E1_prev - fx1/fx1_prime;
+    E1_prev = E1_new;
+end
+E1 = E1_new;
+
+% Now, find true anomaly
+% through algebra, we find that:
+true_anom1_final = acos((cos(E1)-e1)/(1-e1*cos(E1))); % rads
+true_anom1_final_deg = true_anom1_final*180/pi; % convert to degrees
 
 
+% now put it through the orbital elements converter
+[pos_vec1_initial, vel_vec1_initial, spef_energy1_intial] = orbitalElementsToCart(a1, e1, i1, Omega1, w1, true_anom1, mew_earth, "deg");
+[pos_vec1_final, vel_vec1_final, spef_energy1_final] = orbitalElementsToCart(a1, e1, i1, Omega1, w1, true_anom1_final_deg, mew_earth, "deg");
+
+fprintf("Position and Velocity vectors for spacecraft_1 after 1 hour in flight\n")
+fprintf("Initial:\n")
+pos_vec1_initial
+vel_vec1_initial
+fprintf("\nFinal:\n")
+pos_vec1_final
+vel_vec1_final
+
+%%% spacecraft 2
+a2 = 21000;
+e2 = 0.6;
+i2 = 90;
+Omega2 = 0;
+w2 = 0;
+true_anom2 = 35;
+ 
+% first, find E1_0 and mean motion
+E2_0 = acos((e2+cosd(true_anom2))/(1+e2*cosd(true_anom2))*pi/180);
+n2 = sqrt((a2^3)/mew_earth);
+
+% now, solve the equation for E and get a constant
+% Assuming k = 0 for simplicity? not sure how to solve for two unknowns
+k = 0;
+C2 = deltaT/n2 - 2*pi*k + (E2_0 - e2*sin(E2_0));
+E2_guess = pi; % initial guess
+fx2 = C2 - E2_guess + e2*sin(E2_guess); % begin iteration to solve for E
+fx2_prime = -1 + e2*E2_guess*cos(E2_guess);
+E2_prev = E2_guess - fx2/fx2_prime;
+for ii=1:1:100 % 100 iterations should do the trick
+    fx2 = C2 - E2_prev + e2*sin(E2_prev); % begin iteration to solve for E
+    fx2_prime = -1 + e2*E2_prev*cos(E2_prev);
+    E2_new = E2_prev - fx2/fx2_prime;
+    E2_prev = E2_new;
+end
+E2 = E2_new;
+
+% Now, find true anomaly
+% through algebra, we find that:
+true_anom2_final = acos((cos(E2)-e2)/(1-e2*cos(E2))); % rads
+true_anom2_final_deg = true_anom2_final*180/pi; % convert to degrees
+
+
+% now put it through the orbital elements converter
+[pos_vec2_initial, vel_vec2_initial, spef_energy2_intial] = orbitalElementsToCart(a2, e2, i2, Omega2, w2, true_anom2, mew_earth, "deg");
+[pos_vec2_final, vel_vec2_final, spef_energy2_final] = orbitalElementsToCart(a2, e2, i2, Omega2, w2, true_anom2_final_deg, mew_earth, "deg");
+
+fprintf("Position and Velocity vectors for spacecraft_2 after 1 hour in flight\n")
+fprintf("Initial:\n")
+pos_vec2_initial
+vel_vec2_initial
+fprintf("\nFinal:\n")
+pos_vec2_final
+vel_vec2_final
+
+
+%%% Conclusion
+% While (I believe) the above algorithm works, converting it to it's own
+% specific function would be beneficial for general use. However, this
+% current algorithm makes several assumptions. For one, it assumes that the
+% orbits will always be ellipses (or circular), since it does not account
+% for the TOF equation for a parabola or hyperbola. This is something that
+% would need to be added! As well, we are assuming that the true_anom is
+% always less that 180 degrees, and thus are not doing a half-plane check.
+% finally and most important, we are only considering the possiblity that
+% our predictions are relative to one orbit, as in, we are not attempting
+% to solve if the craft makes multiple orbits in the given time (1 hour),
+% rather just where it is relative to the orbit. In order to figure out how
+% many orbits the craft has made as well, we would need to include the K
+% parameter into the equation, rather than just setting it to zero. There
+% is much to be desired for, which I assume is why it's called "Keplers
+% problem" and not "Keplers solution" !!! - Romeo
---
+++ HW5/orbitalElementsToCart.m
@@ -0,0 +1,33 @@
+% Convert PQW states to Cartesian states
+function [position_vec_, velocity_vec_, spef_enegry_] = orbitalElementsToCart(a_, e_, i_param_, omega_param_, w_param_, true_anom_, mew_, deg_or_rad)
+    
+    if deg_or_rad == "deg"
+        i_param_ = i_param_*pi/180;
+        omega_param_ = omega_param_*pi/180;
+        w_param_ = w_param_*pi/180;
+        true_anom_ = true_anom_*pi/180;        
+    end
+    % find r_pqw_vec
+    r = (a_*(1-e_^2)/(1+e_*cos(true_anom_))); % First get current radius at true anomaly
+    r_pqw_vec = [r*cos(true_anom_);r*sin(true_anom_);0]; % Now get Rpqw vector
+    
+    % Find semi-latus rectum (P)
+    p = a_*(1-e_^2);
+    
+    % Get Vpqw
+    v_pqw_vec = sqrt(mew_/p)*[-sin(true_anom_);(e_+cos(true_anom_));0];
+
+    % Get transformation matrix
+    Rz_omega = [cos(-omega_param_), sin(-omega_param_),0; sin(-omega_param_), cos(-omega_param_),0; 0,0,1];
+    Rx_i = [1,0,0; 0, cos(-w_param_), sin(-w_param_); 0, sin(-w_param_), cos(-w_param_)];
+    Rz_w = [cos(-w_param_), sin(-w_param_), 0 ;sin(-w_param_), cos(-w_param_), 0; 0,0,1];
+
+    Tpqw_cart = Rz_omega*Rx_i*Rz_w;
+    r_cart_vec = Tpqw_cart*r_pqw_vec;
+    v_cart_vec = Tpqw_cart*v_pqw_vec;
+
+    position_vec_ = r_cart_vec;
+    velocity_vec_ = v_cart_vec;
+    spef_enegry_ = -mew_/(2*a_);
+    
+end
